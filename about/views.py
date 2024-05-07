66ec6e665a90f034b13434dc3f59788e758ee946
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-05-07 10:30:23 +0200

    Wiring up the URl for about page

--- about/views.py
+++ about/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# Create your views here.
+def about_me(request):
+    return HttpResponse("This is going to be an about page")
